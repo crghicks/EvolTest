dd595ff8005f08be2ae0c134cd4a62acf95bde4c
Hicks, Craig <Craig.Hicks@quintiles.com>
2019-01-10 11:03:23 -0500

    Add project files.

---
+++ evoltest/Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace evoltest.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "My application doesn't do anything but it's dope.";
+        }
+    }
+}
